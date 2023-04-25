9a2fef2b4e5a8dd4d61d31af88e3b648161bf8be
Nitishkulkarni45 <nitishdkulkarnisonu13122001@gmail.com>
2023-04-25 20:13:59 +0530

    location updated

--- src/components/Pagelayout.js
+++ src/components/Pagelayout.js
@@ -4,19 +4,23 @@ import "./Pagelayout.css";
 function Pagelayout() {
     return (
         <div>
-            <h1>Order food to your door</h1>
+            
             <div className="details">
+            <h1>Order food to your door</h1>
+            <div className="detailss">
                 <input type="text" className='location' placeholder='enter delivery address' />
                 <div class="dropdown Drop">
-                    <a class="btn btn-secondary dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a className="btn btn-secondary dropdown-toggle hi" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-clock-fill makeSymbol"></i>
                         Deliver now
                     </a>
-
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/"><i class="bi bi-clock-fill"></i>Deliver now</a></li>
-                        <li><a class="dropdown-item" href="/"><i class="bi bi-calendar-event-fill"></i>Schedule for later</a></li>
+                    <ul class="dropdown-menu" style={{borderRadius:'none !important'}}>
+                        <li><a class="dropdown-item" href="/"><i class="bi bi-clock-fill makeSymbol"></i>Deliver now</a></li>
+                        <li><a class="dropdown-item" href="/"><i class="bi bi-calendar-event-fill makeSymbol"></i>Schedule for later</a></li>
                     </ul>
                 </div>
+                <button type="button" className="btn btn-dark darker">Find Food</button>
+                </div>
+                <div className="end"><a href="/">Sign In</a> for your recent addresses</div>
             </div>
 
         </div>
