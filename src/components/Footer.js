45fdad0d7ddc096067cfd4f160252a8356362ccb
Nitishkulkarni45 <nitishdkulkarnisonu13122001@gmail.com>
2023-05-07 03:23:21 +0530

    whole basic page done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
     <div>
       <div className="foo">
         <div className="headleft">
-          <i class="fa-brands fa-square-facebook"></i>
-          <i class="bi bi-twitter"></i>
-          <i class="bi bi-instagram"></i>
+        <i class="fa-brands fa-square-facebook"></i>
+          <i className="bi bi-twitter"></i>
+          <i className="bi bi-instagram"></i>
         </div>
         <div className="right">
         <div className="right_sub1">
@@ -19,7 +19,7 @@ function Footer() {
         </div>
           <div className="right_sub2">
             <div className="right_sub_sub1">
-              <p>This site is protected by reCAPTCHA and the Google <a href="/">Privacy Policy</a> and <a href="/">Terms of services apply.</a></p>
+              <p className='para'>This site is protected by reCAPTCHA and the Google <a href="/">Privacy Policy</a> and <a href="/">Terms of services apply.</a></p><p className='tech'>©2023 Uber Technologies Inc.</p>
             </div>
             <div className="right_sub_sub2"></div>
           </div>
